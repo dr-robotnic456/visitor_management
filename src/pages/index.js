bcca0bd2e1c69375b7fa5561f06d3f170f5c6177
Ernest Eshun <ernesteshun456@gmail.com>
2023-09-16 13:35:57 +0000

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,34 +8,31 @@ import AddDepartment from './admin/AddDepartment';
 // const secret = 'your-secret-key';
 
 const Home = () => {
-//   const router = useRouter();
+  const router = useRouter();
 
-//   useEffect(() => {
-//     const token = localStorage.getItem('token');
+  useEffect(() => {
+    const token = localStorage.getItem('token');
 
-//     if (token) {
-//       const decodedToken = jwt.decode(token);
+    if (token) {
+      const decodedToken = jwt.decode(token);
 
-//       if (decodedToken.email.includes('@admin.com')) {
-//         router.push('/admin/Dashboard');
-//       } else if (decodedToken.email.includes('@staff.com')) {
-//         router.push('/staff/Dashboard');
-//       } else {
-//         router.push('/UserDashboard');
-//       }
-//     } else {
-//       // Redirect to the login page or handle the absence of a token
-//       // For example, you can use router.push('/login') to redirect to the login page.
-//     }
-//   }, [router]);
+      if (decodedToken.email.includes('@admin.com')) {
+        router.push('/admin/Dashboard');
+      } else if (decodedToken.email.includes('@staff.com')) {
+        router.push('/staff/Dashboard');
+      } else {
+        router.push('/UserDashboard');
+      }
+    } else {
+      // Redirect to the login page or handle the absence of a token
+      // For example, you can use router.push('/login') to redirect to the login page.
+    }
+  }, [router]);
 
-//   // Render a loading indicator or a placeholder while waiting for the redirection
-//   return null;
-// }
+  // Render a loading indicator or a placeholder while waiting for the redirection
+  return null;
+}
 
-return(
-  <AddDepartment />
-)
 }
 
 export default Auth(Home) 
